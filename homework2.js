95c196a73ed526f6757d536d5b1aad805cdb63b8
nastiafill03 <149972381+nastiafill03@users.noreply.github.com>
2026-01-04 12:39:50 +0200

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,88 @@
+// Якщо змінна більше нуля - виведіть true, менше - false //Перевірте це на варіантах 1, 0, -3.
+// let a = -1;
+// if (a >= 1) {
+//   console.log("true");
+// } else if (a <= 0) {
+//   console.log("false");
+// }
+
+// Якщо змінна ="test" - виведіть true, //Перевірте це на варіантах 'test', "qwerty", true
+// let a = "qwerty";
+// if (a === "test") {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// Якщо змінна більше 10 - відніміть 5, //менше - додайте 5, результат виведіть в консоль //Перевірте це на варіантах 1, 10, 13.
+// let a = 1;
+// if (a >= 11) {
+//   console.log(a - 5);
+// } else {
+//   console.log(a + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12 // виведіть місяць який дорівнює числу
+// let month = prompt("Введіть число від 1 до 12");
+// switch (month) {
+//   case "1":
+//     if (month === 1);
+//     console.log("січень");
+//     break;
+//   case "2":
+//     if (month === 2);
+//     console.log("лютий");
+//     break;
+//   case "3":
+//     if (month === 3);
+//     console.log("березень");
+//     break;
+//   case "4":
+//     if (month === 4);
+//     console.log("квітень");
+//     break;
+//   case "5":
+//     if (month === 5);
+//     console.log("травень");
+//     break;
+//   case "6":
+//     if (month === 6);
+//     console.log("червень");
+//     break;
+//   case "7":
+//     if (month === 7);
+//     console.log("липень");
+//     break;
+//   case "8":
+//     if (month === 8);
+//     console.log("серпень");
+//     break;
+//   case "9":
+//     if (month === 9);
+//     console.log("вересень");
+//     break;
+//   case "10":
+//     if (month === 10);
+//     console.log("жовтень");
+//     break;
+//   case "11":
+//     if (month === 11);
+//     console.log("листопад");
+//     break;
+//   case "12":
+//     if (month === 12);
+//     console.log("грудень");
+//     break;
+// }
+
+//Зробіть сервіс який отримує тризначне число //Поверніть користувачу сумму цих чисел
+// let num = prompt("Введіть тризначне число ");
+// parseInt(num);
+// let a = Math.floor(num / 100);
+// let b = Math.floor((num % 100) / 10);
+// let c = num % 10;
+// if (num >= 100 && num <= 999) {
+//   console.log(a + b + c);
+// } else {
+//   console.log("Reset");
+// }
